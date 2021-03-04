eca2cd701a04f500d786d4212db38865587749dc
Billy Yip <birii.yip@gmail.com>
2021-03-04 15:45:47 -0800

    included button + git card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
 
 function App() {
+  const [display, setDisplay] = useState(false);
+
+  const [user, setUser] = useState({});
+
+  const handleClick = () => {
+    setDisplay(!display);
+    fetch("https://api.github.com/users/darkcloudb")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+    console.log(user);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React NAO
-        </a>
-      </header>
+      <button onClick={handleClick} type="button" class="btn btn-primary">
+        Toggle User
+      </button>
+      <p></p>
+
+      {display ? (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://avatars.githubusercontent.com/u/73850400?v=4"
+          />
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Card.Text>
+              <p>Followers: {user.followers}</p>
+              <p>Created: {user.created_at}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ) : (
+        " "
+      )}
     </div>
   );
 }
